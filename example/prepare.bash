231056517b4cdc3c8549153605868ffa38d30b9a
douzengrui <douzengrui@gmail.com>
2025-02-10 15:46:13 +0800

    修改 prepare.bash 方便测试

--- example/prepare.bash
+++ example/prepare.bash
@@ -1,13 +1,22 @@
 kubectl create ns redis-operators
 
-#kubectl apply --server-side -f ../config/crd/bases/
+kubectl apply --server-side -f config/crd/bases/
+
+helm package redis-operator
 
 helm install redis-operator repo/redis-operator --namespace redis-operators
 
-#helm install redis-sentinel repo/redis-sentinel \
-#  --set redissentinel.clusterSize=3  --namespace redis-operators \
-#  --set redisSentinelConfig.redisReplicationName="redis-replication"
+helm uninstall redis-operator --namespace redis-operators
 
+curl --data-binary "@redis-operator-0.18.5.tgz" http://localhost:8080/api/charts
+
+curl -X DELETE http://localhost:8080/api/charts/redis-operator/0.18.5
+
+helm install redis-sentinel repo/redis-sentinel \
+  --set redissentinel.clusterSize=3  --namespace redis-operators \
+  --set redisSentinelConfig.redisReplicationName="redis-replication"
+
+helm uninstall redis-sentinel --namespace redis-operators
 
 mkdir -p /tmp/k8s-webhook-server/serving-certs/
 cp tls.crt /tmp/k8s-webhook-server/serving-certs/
